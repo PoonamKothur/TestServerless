28ff083b2907d51530838ce68f334c8f7b8f27d2
Poonam <poonam@codeblock.co.in>
2019-10-29 22:02:51 +0530

    err in fun

--- apis/students/getStudents.js
+++ apis/students/getStudents.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler.list = async event => {
+module.exports.list = async event => {
     const params = {
         TableName: "Students"
     };
