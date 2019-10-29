6f852b139b783fa33ee114bc9b1f50ed16edb53b
Poonam <poonam@codeblock.co.in>
2019-10-29 07:49:15 +0530

    commiting changes firsttime in git

---
+++ apis/students/addStudents.js
@@ -0,0 +1,44 @@
+
+
+const AWS = require("aws-sdk");
+
+const generateUStudentid = Math.random() * 1000;
+
+//initialse dynamoDB
+const documentCLient = new AWS.DynamoDB.DocumentClient();
+
+
+const insertStudent = async (body) => {
+    const params = {
+        TableName: "Students",
+        Item: {
+            StudentId: generateUStudentid,
+            FirstName: body.firstName
+        }
+    };
+
+    return await documentCLient.put(params).promise();
+};
+
+exports.handler = async event => {
+    
+    const  body = JSON.parse(event.body);
+
+    try {
+        const data = await insertStudent(body);
+ 
+        const response = {
+            statusCode: 200
+        };
+        return response;
+    } 
+    catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        };
+    }
+    return {
+        statusCode: 500
+    };
+}
---
+++ apis/students/getStudents.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async event => {
+    const params = {
+        TableName: "Students"
+    };
+    try {
+        const data = await documentClient.scan(params).promise();
+        if(data){
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        };
+        return response;
+        }
+    } catch (e) {
+        console.log(e);
+       
+    }
+    
+    return {
+        statusCode: 500
+    };
+};
